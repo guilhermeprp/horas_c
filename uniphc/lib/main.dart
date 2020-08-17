f802c134c5430dd3312974feee511aa031359828
guilherme.pereira <guilherme.pereira@ebc.com.br>
2020-08-17 02:31:43 -0300

    Orientation lock on main

--- uniphc/lib/main.dart
+++ uniphc/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:uniphc/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'app/shared/orientation/orientation.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() {
+  runApp(ModularApp(module: AppModule()));
+  portraitModeOnly();
+}
